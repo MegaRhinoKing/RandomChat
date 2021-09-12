1b88fe76067bc76fa51a6a360cfdd9ebccb4df80
MegaRhinoKing <41524102+MegaRhinoKing@users.noreply.github.com>
2021-09-12 21:03:40 +0100

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,71 @@
+const socket = io.connect("http://localhost:3000")
+
+let selfid = undefined
+let strangerid = undefined
+
+const chatbox = document.getElementById("chatbox")
+const connection = document.getElementById("connectionstatus")
+const msgbox = document.getElementById("msgbox")
+
+socket.on("stranger_found", (arg)=>{
+    if (arg.userid == selfid) {
+         strangerid = arg.strangerid
+    }
+    else if (arg.strangerid == selfid) {
+        strangerid = arg.userid
+    }
+
+    document.title = "Stranger found!"
+    connection.innerText = "Stranger found!"
+
+    console.log("strangerid: " + strangerid + ". selfid:", selfid)
+
+    msgbox.disabled = false
+
+    socket.emit("new_entry", {[arg.userid]:strangerid})
+})
+
+socket.on("your_id", (arg) => {
+    selfid = arg
+})
+
+socket.on("message", (arg) => {
+    const author = arg.author
+    const message = arg.message
+    let sender_type = undefined
+
+    if (author==selfid) {
+        sender_type = "You: "
+    }
+    else if (author==strangerid) {
+        sender_type = "Stranger: "
+    }
+
+    let para = document.createElement("P")
+    para.innerText = sender_type+message
+    document.getElementById("messages").appendChild(para)
+})
+
+socket.on("chat_ended", (arg) => {
+    let para = document.createElement("P")
+    para.innerText = "Stranger disconnected..."
+    document.getElementById("messages").appendChild(para)
+    msgbox.disabled = true
+})
+
+chatbox.onsubmit = (e)=>{
+    e.preventDefault();
+    console.log(e)
+    const text = e.target[0].value
+    e.target[0].value = ""
+
+    socket.emit("message_to_server", {msg_details:{message:text,author:selfid},strangerid})
+
+    //io.to(selfid).to(strangerid).emit("message", )
+}
+
+newchat.onclick = (e)=>{
+    socket.emit("chat_left", strangerid)
+    window.location.replace("/newchat");
+}
+
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const http = require('http')
+var socket = require("socket.io")
+const cookieParser = require('cookie-parser');
+const cookie = require("cookie");
+const { S_IFMT } = require('constants');
+
+const app = express()
+
+app.use(cookieParser())
+
+const server = app.listen(3000, ()=> console.log("Listening on port 3000."))
+
+app.use(express.static("public"))
+
+const io = socket(server)
+
+let waiting = []
+let open = []
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/templates/homepage.html");
+    
+    let id = Math.floor(Math.random() * 100000000000) + 1;
+    res.cookie("userid", id)
+  });
+ 
+  app.get("/newchat", (req, res) => {
+    res.sendFile(__dirname + "/templates/newchat.html");
+    let connected = false;
+
+    userid = req.cookies.userid
+    waiting.push(userid)
+    let strangerid = null
+
+    for (let i = 0; i < waiting.length; i++) {
+      if (waiting[i]!==userid) {
+        strangerid = waiting[i]
+        let index= waiting.indexOf(waiting[i])
+        if (index > -1) {
+          waiting.splice(index, 1);
+        }
+        index = waiting.indexOf(userid)
+        if (index > -1) {
+          waiting.splice(index, 1);
+
+        setTimeout(()=> {
+          io.to(userid).to(strangerid).emit("stranger_found", {userid,strangerid})
+          console.log(userid, strangerid, "message emited")
+        }, 2000)
+      }
+      }}
+
+  })
+
+io.on("connection", function(socket){
+    console.log('made socket connection,', socket.id)
+
+    let cookief = socket.handshake.headers.cookie; 
+    let cookies = cookie.parse(socket.handshake.headers.cookie);   
+    let userid = cookies.userid
+  
+    socket.join(userid)
+    io.to(userid).emit("your_id",userid)
+    console.log(userid, "room joined")
+
+    socket.on("message_to_server", (arg) => {
+      const message = arg.msg_details.message
+      const author = arg.msg_details.author
+
+      console.log(message, author)
+
+      io.to(arg.msg_details.author).to(arg.strangerid).emit("message", {message,author})
+    })
+
+    socket.on("chat_left", (arg) => {
+      io.to(arg).emit("chat_ended", 1)
+    })
+
+    socket.on("new_entry", (arg) => {
+      console.log("new entry",arg)
+      open.push(arg)
+    })
+
+    socket.on("disconnecting", () => {
+      console.log("disconnected",open)
+      for (let i=0; i<open.length; i++) {
+        for (const [key, value] of Object.entries(open[i])) {
+          if (key==userid) {
+            io.to(value).emit("chat_ended")
+          }
+        }
+
+      }
+      
+    })
+});
+
